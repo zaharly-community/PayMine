4b7f60080b063332a5b3a58a7a3b9454063b705d
Pay <zaharly@proton.me>
2026-09-18 18:35:12 +0100

    Delete Issues & Investigations route

--- src/routes/(main)/dashboard/issues/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { IssuesPage } from "./-components/issues-page";
-
-export const Route = createFileRoute("/(main)/dashboard/issues/")({
-  component: IssuesPage,
-});
