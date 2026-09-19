ab1b9eec8a368ffef615e7127c6b94c4bb2d584f
Pay <zaharly@proton.me>
2026-09-19 16:08:05 +0100

    remove old transaction detail route

--- src/routes/(main)/dashboard/deposits/transaction-detail/route.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { TransactionDetail } from "../-components/transaction-detail";
-
-export const Route = createFileRoute(
-  "/(main)/dashboard/deposits/transaction-detail",
-)({
-  component: TransactionDetail,
-});
