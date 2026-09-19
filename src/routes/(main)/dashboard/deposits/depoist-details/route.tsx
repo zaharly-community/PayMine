b2ccc6f62f93856137cbb37620b86d383f895cb7
Pay <zaharly@proton.me>
2026-09-19 16:07:41 +0100

    rename deposit transaction detail route

---
+++ src/routes/(main)/dashboard/deposits/depoist-details/route.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+import { TransactionDetail } from "../-components/transaction-detail";
+
+export const Route = createFileRoute(
+  "/(main)/dashboard/deposits/depoist-details",
+)({
+  component: TransactionDetail,
+});
