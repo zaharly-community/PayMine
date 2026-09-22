d20659b71a3fad1417a3e42254c618a501c41269
Pay <zaharly@proton.me>
2026-09-22 11:07:33 +0100

    Remove dashboard desktop padding

--- src/routes/(main)/dashboard/route.tsx
+++ src/routes/(main)/dashboard/route.tsx
@@ -71,7 +71,7 @@ function DashboardLayout() {
           </div>
         </header>
         {/* Pages can set data-content-padding="false" to render full-bleed app layouts. */}
-        <div className="min-h-0 min-w-0 flex-1 overflow-x-hidden p-4 has-data-[content-padding=false]:p-0 md:p-6 md:has-data-[content-padding=false]:p-0">
+        <div className="min-h-0 min-w-0 flex-1 overflow-x-hidden p-4 has-data-[content-padding=false]:p-0 md:has-data-[content-padding=false]:p-0">
           <Outlet />
         </div>
       </SidebarInset>
