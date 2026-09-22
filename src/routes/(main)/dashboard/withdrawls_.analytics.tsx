217e0ce6c792d35a505f14202a43898247d9ad2e
Pay <zaharly@proton.me>
2026-09-22 13:48:17 +0100

    Remove duplicate withdrawls analytics route

--- src/routes/(main)/dashboard/withdrawls_.analytics.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { WithdrawlsAnalytics } from "./withdrawls/-components/withdrawls-analytics";
-
-export const Route = createFileRoute("/(main)/dashboard/withdrawls_/analytics")({
-  component: WithdrawlsAnalytics,
-});
