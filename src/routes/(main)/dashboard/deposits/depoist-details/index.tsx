f6c7e23ec51221fadd6ad2257d1f704ad454fd48
Pay <zaharly@proton.me>
2026-09-24 16:06:45 +0100

    Fix wallet deposit detail import

--- src/routes/(main)/dashboard/deposits/depoist-details/index.tsx
+++ src/routes/(main)/dashboard/deposits/depoist-details/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { TransactionDetail } from "../../-components/transaction-detail";
+import { TransactionDetail } from "../-components/transaction-detail";
 
 export const Route = createFileRoute(
   "/(main)/dashboard/deposits/depoist-details/",
