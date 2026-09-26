1a973128b4926a6d5214187585251b498ce7b05f
Pay <zaharly@proton.me>
2026-09-26 14:12:42 +0100

    Replace ooredoo static portal route with dynamic method route

--- src/routes/(main)/portal/ooredoo/route.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { CustomerPortal } from "../-components/customer-portal";
-
-export const Route = createFileRoute("/(main)/portal/ooredoo")({
-  component: () => <CustomerPortal initialMethodId="ooredoo" />,
-});
