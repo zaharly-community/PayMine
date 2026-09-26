d0d120ff86bc949432b35ce7f1ede80871866776
Pay <zaharly@proton.me>
2026-09-26 14:12:14 +0100

    Replace d17 static portal route with dynamic method route

--- src/routes/(main)/portal/d17/route.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { CustomerPortal } from "../-components/customer-portal";
-
-export const Route = createFileRoute("/(main)/portal/d17")({
-  component: () => <CustomerPortal initialMethodId="d17" />,
-});
