395b230ab3edde9511749eddf1a60721b6fa8ae8
Pay <zaharly@proton.me>
2026-09-26 14:12:22 +0100

    Replace kashy static portal route with dynamic method route

--- src/routes/(main)/portal/kashy/route.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { CustomerPortal } from "../-components/customer-portal";
-
-export const Route = createFileRoute("/(main)/portal/kashy")({
-  component: () => <CustomerPortal initialMethodId="kashy" />,
-});
